9ee7efc8817bafbacb629a634dd0386f908b3c3b
Alex Nicholas <al.nicholas@gmail.com>
2020-07-01 15:31:36 +0100

    Server setup

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const Router = require('./router')
+const port = 8000
+
+mongoose.connect('mongodb://localhost/wildernessdb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+}, (err) => {
+  err ? console.log(err) : console.log('Mongoose connected')
+})
+
+const expressServer = express()
+
+expressServer.use((req, res, next) => {
+  console.log(`Incoming ${req.method} to ${req.url}`)
+  next()
+})
+
+expressServer.use(bodyParser.json())
+
+// expressServer.use('/api/', Router)
+
+expressServer.listen(port, () => console.log(`Running on port ${port}`))
+
+module.exports = expressServer
