49417424c8938f816e8093492692d2e61c303b9e
Ali Bhimani <ali_bhimani21@aol.com>
2020-07-08 23:14:51 +0100

    Styling

--- src/components/MyAccount.js
+++ src/components/MyAccount.js
@@ -4,6 +4,8 @@ import { UserContext } from './Context'
 import { Link } from 'react-router-dom'
 import { ReviewListItem } from './ReviewList'
 import { SiteList } from './SiteList'
+import { Settings } from './Settings'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'
 
 export const MyAccount = () => {
 
@@ -32,24 +34,23 @@ export const MyAccount = () => {
         setEditingBio(false)
       }
       )
-      
+
       .catch(error => console.log(error))
   }
 
   if (!userDetails) return <h1>Loading...</h1>
-  
+
   return (
     <section className="my-account">
-      <Link to='/account/settings'>Settings</Link>
       <h1>My Account</h1>
       <h2>{userDetails.firstName} {userDetails.lastName}</h2>
-      <img src={userDetails.avatar} alt="user avatar"/>
+      <img src={userDetails.avatar} alt="user avatar" />
       <Link to='account/settings'><p>Change avatar</p></Link>
       <h3>My bio:</h3>
       {userDetails.bio ?
         <>
-        <p>{userDetails.bio}</p>
-        <span onClick={() => setEditingBio(true)}>Edit bio</span>
+          <p>{userDetails.bio}</p>
+          <span onClick={() => setEditingBio(true)}>Edit bio</span>
         </>
         :
         <p>No bio yet...<span onClick={() => setEditingBio(true)}>add one</span></p>
@@ -59,47 +60,72 @@ export const MyAccount = () => {
         <textarea id="edit-bio" name="bio" value={bio} onChange={(e) => setBio(e.target.value)}></textarea>
         <button onClick={handleClick}>Save</button>
       </form>}
-      {/* TABS */}
-      {currentUser.showWishList && <>
-        <h3>Places I want to go:</h3>
-        {(userDetails.recAreaWishList.length || userDetails.campgroundWishList.length) ?
-          <div className="wish-list">
-            <div className="rec-area-wish-list">
-              {userDetails.recAreaWishList.map((recArea, i) => <SiteList key={i} site={recArea} />)}
-            </div>
-            <div className="campground-wish-list">
-              {userDetails.campgroundWishList.map((campground, i) => <SiteList key={i} site={campground} />)}
-            </div>
-          </div> :
-          <p>You haven&apos;t put any places on your wish list yet. Just click on the heart to add a recreational area or campground to your list.</p>
-        }
-      </>}
-      {currentUser.showVisited && <>
-      <h3>Places I&apos;ve been:</h3>
-      {(userDetails.recAreasVisited.length || userDetails.campgroundsVisited.length) ?
-        <div className="visited">
-          <div className="rec-areas-visited">
-            {userDetails.recAreasVisited.map((recArea, i) => <SiteList key={i} site={recArea} />)}
-          </div>
-          <div className="campgrounds-visited">
-            {userDetails.campgroundsVisited.map((campground, i) => <SiteList key={i} site={campground} />)}
-          </div>
-        </div> :
-        <p>You haven&apos;t marked any places as visited yet. Just click on the tick to add a recreational area or campground to your list of visited places.</p>
-      }
-      </>}
-      <h3>My reviews:</h3>
-      {(userDetails.recAreaReviews.length || userDetails.campgroundReviews.length) ?
-        <div className="my-reviews">
-          <div className="rec-area-reviews">
-            {userDetails.recAreaReviews.map((review, i) => <ReviewListItem key ={i} review={review} displayName={false} enableComments={false} displayAvatar={false}/>)}
-          </div>
-          <div className="campground-reviews">
-            {userDetails.campgroundReviews.map((review, i) => <ReviewListItem key ={i} review={review} displayName={false} enableComments={false} displayAvatar={false}/>)}
-          </div>
-        </div> :
-        <p>You haven&apos;t left any reviews yet. Why not add one for somewhere you&apos;ve been?</p>
-      }
+
+      <Tabs>
+        <TabList>
+          <Tab>My Locations</Tab>
+          <Tab>My Reviews</Tab>
+          <Tab>My Settings</Tab>
+        </TabList>
+        <TabPanel>
+          <Tabs>
+            <TabList>
+              <Tab>My Wishlist</Tab>
+              <Tab>Places I&apos;ve Visited</Tab>
+            </TabList>
+            <TabPanel>
+              {currentUser.showWishList && <>
+                <h3>Places I want to go:</h3>
+                {(userDetails.recAreaWishList.length || userDetails.campgroundWishList.length) ?
+                  <div className="wish-list">
+                    <div className="rec-area-wish-list">
+                      {userDetails.recAreaWishList.map((recArea, i) => <SiteList key={i} site={recArea} />)}
+                    </div>
+                    <div className="campground-wish-list">
+                      {userDetails.campgroundWishList.map((campground, i) => <SiteList key={i} site={campground} />)}
+                    </div>
+                  </div> :
+                  <p>You haven&apos;t put any places on your wish list yet. Just click on the heart to add a recreational area or campground to your list.</p>
+                }
+              </>}
+            </TabPanel>
+            <TabPanel>
+              {currentUser.showVisited && <>
+                <h3>Places I&apos;ve been:</h3>
+                {(userDetails.recAreasVisited.length || userDetails.campgroundsVisited.length) ?
+                  <div className="visited">
+                    <div className="rec-areas-visited">
+                      {userDetails.recAreasVisited.map((recArea, i) => <SiteList key={i} site={recArea} />)}
+                    </div>
+                    <div className="campgrounds-visited">
+                      {userDetails.campgroundsVisited.map((campground, i) => <SiteList key={i} site={campground} />)}
+                    </div>
+                  </div> :
+                  <p>You haven&apos;t marked any places as visited yet. Just click on the tick to add a recreational area or campground to your list of visited places.</p>
+                }
+              </>}
+            </TabPanel>
+          </Tabs>
+        </TabPanel>
+        <TabPanel>
+          <h3>My reviews:</h3>
+          {(userDetails.recAreaReviews.length || userDetails.campgroundReviews.length) ?
+            <div className="my-reviews">
+              <div className="rec-area-reviews">
+                {userDetails.recAreaReviews.map((review, i) => <ReviewListItem key={i} review={review} displayName={false} enableComments={false} displayAvatar={false} />)}
+              </div>
+              <div className="campground-reviews">
+                {userDetails.campgroundReviews.map((review, i) => <ReviewListItem key={i} review={review} displayName={false} enableComments={false} displayAvatar={false} />)}
+              </div>
+            </div> :
+            <p>You haven&apos;t left any reviews yet. Why not add one for somewhere you&apos;ve been?</p>
+          }
+        </TabPanel>
+        <TabPanel>
+          <Settings />
+        </TabPanel>
+      </Tabs>
+
     </section>
   )
 }
