c6033c2d01c73ef72d3bf884ded07a2a8dd8e369
Alex Nicholas <al.nicholas@gmail.com>
2020-08-13 23:10:05 +0100

    deployment fixes

--- server/config/environment.js
+++ server/config/environment.js
@@ -7,7 +7,7 @@ const dbURI = env === 'production'
   ? process.env.ATLASDB_URI
   : `mongodb://localhost/wildernessdb-${env}`
 
-
 const secret = 'campgrounds are the same as campsites and area 51 is a rec area but trump did not want you to know'
 
 module.exports = { port, dbURI, secret }
+
